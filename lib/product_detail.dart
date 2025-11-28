6b11eefdcb6c167649f828758129d60e0d63c067
Salih Efe Şerit <salihefeserit@gmail.com>
2025-11-28 17:49:22 +0300

    id taşıma

--- lib/product_detail.dart
+++ lib/product_detail.dart
@@ -3,20 +3,27 @@ import 'package:urunkatalog/product_model.dart';
 
 class ProductDetail extends StatelessWidget {
   final int prodid;
+
   const ProductDetail({super.key, required this.prodid});
 
   @override
   Widget build(BuildContext context) {
     final Product urun = products.firstWhere((urun) => urun.id == prodid);
     return Scaffold(
-      appBar: AppBar(title: Text(urun.ad), backgroundColor: Colors.lightGreen,),
+      appBar: AppBar(title: Text(urun.ad), backgroundColor: Colors.lightGreen),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Text('ID: ${urun.id}', style: Theme.of(context).textTheme.headlineMedium),
+            Text(
+              'ID: ${urun.id}',
+              style: Theme.of(context).textTheme.headlineMedium,
+            ),
             const SizedBox(height: 10),
-            Text('Fiyat: ${urun.ucret} TL', style: Theme.of(context).textTheme.titleLarge),
+            Text(
+              'Fiyat: ${urun.ucret} TL',
+              style: Theme.of(context).textTheme.titleLarge,
+            ),
             const SizedBox(height: 30),
             ElevatedButton(
               onPressed: () {
