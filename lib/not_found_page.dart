9cab3b9511518ee02d11036e9ef40905c84ece3d
Salih Efe Şerit <salihefeserit@gmail.com>
2025-11-28 17:49:45 +0300

    id taşıma

--- lib/not_found_page.dart
+++ lib/not_found_page.dart
@@ -12,17 +12,17 @@ class NotFoundPage extends StatelessWidget {
   Widget build(BuildContext context) {
     debugPrint(message);
     return Scaffold(
-      appBar: AppBar(title: const Text('404 - Hata'), backgroundColor: Colors.lightGreen,),
+      appBar: AppBar(
+        title: const Text('404 - Hata'),
+        backgroundColor: Colors.lightGreen,
+      ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             const Icon(Icons.error_outline, color: Colors.red, size: 60),
             const SizedBox(height: 20),
-            Text(
-              message,
-              textAlign: TextAlign.center,
-            ),
+            Text(message, textAlign: TextAlign.center),
             const SizedBox(height: 20),
             ElevatedButton(
               onPressed: () => Navigator.pop(context),
